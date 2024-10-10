30579f44c34274b91d1bb80fa68aabda36dca585
Fabian Sainz <sainzfabian01@gmail.com>
2024-10-09 20:53:57 -0600

    09/10/24

---
+++ src/app/formularios/zodiaco/zodiaco.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zodiaco',
+  standalone: true,
+  imports: [],
+  templateUrl: './zodiaco.component.html',
+  styleUrl: './zodiaco.component.css'
+})
+export class ZodiacoComponent {
+
+  
+}
